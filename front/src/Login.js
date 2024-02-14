1416cd3f951e814c9bdd10a862b5c8072cfc6a45
damirbeybitov <damirbey7@gmail.com>
2024-02-14 12:46:36 +0600

    sender to back from login page email, password

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,10 +1,20 @@
 import './css/login.css';
+import { sendDataToBackend } from './components/Button';
 
 const Login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+      
+        // Send the data to your Go back-end
+        sendDataToBackend(email, password);
+    };
+
     return (
         <div>
             <div class="form-wrapper"> <h1>Log in</h1>
-                <form action="#">
+                <form handleSubmit={handleSubmit} action="#">
                     <div class="input-field">
                         <label for="email">Email address:</label>
                         <input type="email" id="email" name="email" placeholder="Email address"></input>
