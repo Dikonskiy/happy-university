9c72022187a1f00245811f72ebfa09ba76e7b1c8
Arystanbayev Dias <dias.arystanbaev16@gmail.com>
2024-02-28 10:17:51 +0600

    update create table query

--- queries/users.sql
+++ queries/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT null,
-    email varchar(255) not null,
+    email varchar(255) not null unique,
     role varchar(255) not null,
     password VARCHAR(255) NOT NULL
 );
