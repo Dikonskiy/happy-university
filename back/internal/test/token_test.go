aae2c9b1256762ff8a469913e274cb7f356fa45b
cucumber4 <g.chasovnikov@a1qa.com>
2024-04-03 16:42:27 +0500

    config, token, logger unit tests

---
+++ back/internal/test/token_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Dikonskiy/happy-university/back/internal/token"
+)
+
+func TestGenerateTokens(t *testing.T) {
+	cardId := " "
+	role := " "
+
+	accessToken, refreshToken, err := token.GenerateTokens(cardId, role)
+	if err != nil {
+		t.Fatalf("Ошибка при генерации токенов: %v", err)
+	}
+
+	if accessToken == "" {
+		t.Error("AccessToken не должен быть пустым")
+	}
+
+	if refreshToken == "" {
+		t.Error("RefreshToken не должен быть пустым")
+	}
+}
